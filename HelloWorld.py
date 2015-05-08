2b134042053489823f7c1a7a35da8e131e5ec088
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-08 09:51:27 -0400

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+print "Hello World"
+print "My name is Karin \nThis is Python"
+print 2015 #printing an integer
+print "2015" #Printing a string
+print  3.1416 #Printing a floating point number 
+
+print 2 + 2
+print 2 - 2
+print 2 * 2
+print 2 / 2
+
+print 3 % 2 #Modulus is the remainder of a division 
+print 10 % 3
+
+print 3.1416 * 2.0
+print 3.1416 * 2
