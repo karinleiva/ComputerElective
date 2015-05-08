3d868d687f4bc1e693ae876dcddc4bd9a086d5fb
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-08 09:52:46 -0400

    Create PersonalVariables.py

---
+++ PersonalVariables.py
@@ -0,0 +1,18 @@
+name= "Karin Leiva"
+age = 16
+gpa = 4.0
+email = "16kleiva@iaa.edu.ec"
+telephonenumber = "0969785422"
+address = "Cdla Los Parques"
+
+print name
+print age 
+print gpa
+print email
+print telephonenumber
+print address
+
+print 'My name is', name ,'. I am ' , age , 'years old.'
+print 'My email is', email 
+print 'My telephone number is', telephonenumber
+print 'My adress is' , address 
