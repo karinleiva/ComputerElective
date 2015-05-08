2bfe9609dfb01a6850cc29f7da974b9d2e9bdb45
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-08 09:52:29 -0400

    Create ClassActivity.py

---
+++ ClassActivity.py
@@ -0,0 +1,20 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+print mystring 
+print myfloat
+print myint
+
+print 'My string is', mystring, 'my integer is' , myint , 'my floating point is' , myfloat ,
+
+print 8 * 2 + 4
+print 10 / 10
+print 9 + 3 / 3
+print 12 + 5 + 10
+print 66 / 11 + 2
+
+import math 
+root = math.sqrt(16)
+print root
+
