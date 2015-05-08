2e65cd7c02b2b840da506a4754d5041d0ad2f8b5
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-08 09:51:48 -0400

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+name = "Karin Leiva"
+age = 16
+gpa = 4.0
+
+print name
+print age
+print gpa
+
+print 'My name is', name, '. I am ' , age , 'years old.'
+
+print "In ten years, i will be" , age + 10
+
+#Dog years
+ageinHumanYears = 6
+ageInDogYears = ageinHumanYears * 7
+print 'My Dog is' , ageInDogYears, 'years old in Dog Years.'
+print 'But my dog is' , ageinHumanYears, 'years old in Human Years.'
+
+
+#Order of Operations
+#Parenthsis, Exponents, Multiplicaiton, Division, Addition, Substitution 
+MyNumber = 5 + 4 * 5
+print MyNumber 
