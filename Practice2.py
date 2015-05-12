6c6c34784b915bf45fedc4d568f58d470e87674d
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-12 10:00:04 -0400

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,17 @@
+import sys
+
+#print "You are using command line arguments:", sys. argv
+#print "The first argument you entered is:", sys. argv[1]
+#print "The second argument you entered is:", sys.argv[2]
+#print "The third argument you entered is:", sys.argv[3]
+#print "The fourth argument you entered is:", sys.argv[4]
+#print "The fifth argument you entered is:", sys.argv[5]
+#print "The sixth argument you entered is:", sys.argv[6]
+
+print 'My first name is', sys.argv[1]
+print 'My last name is', sys.argv[2]
+print 'My year of birth is',sys.argv[3]
+print 'My favorite singer is',sys.argv[4]
+print 'My favorite movie is',sys.argv[5]
+print 'My best friend is',sys.argv[6]
+
