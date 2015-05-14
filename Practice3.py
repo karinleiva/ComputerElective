a8e3c12bd37fa2148ff7320cad2001eeffa5ea13
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-14 09:57:16 -0400

    Create Practice3.py

---
+++ Practice3.py
@@ -0,0 +1,13 @@
+name= raw_input("What is your name?")
+lastname = raw_input("What is your last name?")
+age = input ("How old are you?")
+singer = raw_input ("What is your favorite singer?")
+movie= raw_input ("What is your favorite movie?")
+bestfriend=raw_input ("Who is your best friend?")
+
+
+print name, "You are", age , "years old"
+
+age = age + 10
+print name, "you are" , age , "years old in ten years"
+print name * 10
