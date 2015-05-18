c598f043ea68bb424138c98bcb3b3487c9cb5677
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-18 09:50:00 -0400

    Create Quiz2.py

---
+++ Quiz2.py
@@ -0,0 +1,10 @@
+name= raw_input("What is your name?")
+lastname = raw_input("What is your last name?")
+Class = raw_input("What class are you in?")
+year = raw_input("What year were you born in?")
+age = input ("How old are you?")
+first = input("What's your first semester grade?")
+second= input("What's your second semester grade?")
+final= input ("What's your final grade?") 
+
+print name, "You are", age , "years old"  , "For your Computer Class" , Class , "Your final grade is", final , 
