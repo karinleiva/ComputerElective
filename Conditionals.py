e6ae9bb9ded54e6eb29bb22c871da496b027b57a
Karin Leiva <16kleiva@iaa.edu.ec>
2015-05-20 09:38:50 -0400

    Create Conditionals.py

---
+++ Conditionals.py
@@ -0,0 +1,18 @@
+num = input("Integer?")
+
+if num < 0:
+    print "Your number is less than 0"
+    #print "Your number is" , num 
+else:
+    print "Your number is greater than 0"
+    #print "Your number is" , num
+    
+print "Your number is" , num
+
+#Operators for conditionals - Python 
+#Less than : <
+#Greater than : >
+#Equal : ==
+#Not Equal : !=
+#Less than or equal to : <=
+#Greater than or equal to : >=
